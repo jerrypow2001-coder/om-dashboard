c6a19fd9325c8394594413b1475e0f06b99edc28
jerrypow2001-coder <jerrypow2001@gmail.com>
2025-12-05 10:15:18 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,29 @@
-const CACHE_NAME = 'om-dash-v1';
-    const URLS_TO_CACHE = [
-      './',
-      './index.html',
-      './manifest.json',
-      './icon.png',
-      'https://unpkg.com/react@18/umd/react.development.js',
-      'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
-      'https://unpkg.com/@babel/standalone/babel.min.js',
-      'https://cdn.tailwindcss.com',
-      'https://cdn.sheetjs.com/xlsx-latest/package/dist/xlsx.full.min.js'
-    ];
+const CACHE_NAME = 'om-dashboard-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.development.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
 
-    self.addEventListener('install', (event) => {
-      event.waitUntil(
-        caches.open(CACHE_NAME)
-          .then((cache) => cache.addAll(URLS_TO_CACHE))
-      );
-    });
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
 
-    self.addEventListener('fetch', (event) => {
-      event.respondWith(
-        caches.match(event.request)
-          .then((response) => response || fetch(event.request))
-      );
-    });
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
